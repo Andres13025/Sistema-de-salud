98fd4b124a5cf98ae624d5c1f57080dceb2c6c67
Carlos <carlosandresvalerovelasco@gmail.com>
2022-10-05 19:50:21 -0500

    incio

---
+++ Back/BE-SistemaSalud/WebApplication1/Models/DatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+        public virtual DbSet<TiposCondiciones> TiposCondiciones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasNoKey();
+                entity.ToTable("Usuario");
+                entity.Property(e => e.Id_Usuario).HasColumnName("Id_usuario");
+                entity.Property(e => e.Nombre).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.Correo).HasMaxLength(50).IsUnicode(false);
+                entity.Property(e => e.Contraseña).HasMaxLength(100).IsUnicode(false);
+                entity.Property(e => e.Fecha_De_Creacion).IsUnicode(false);
+                entity.Property(e => e.Activo).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TiposCondiciones>(entity =>
+            {
+                entity.ToTable("TiposCondiciones");
+                entity.Property(e => e.Id_Condicion_De_Salud).HasColumnName("Id_Condicion_De_Salud");
+                entity.Property(e => e.Descripcion).HasMaxLength(150).IsUnicode(false);
+                entity.Property(e => e.Fecha_Emision).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Back/BE-SistemaSalud/WebApplication1/Models/TiposCondiciones.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class TiposCondiciones
+    {
+        [Key]
+        public int Id_Condicion_De_Salud { get; set; }
+        public string? Descripcion { get; set; }
+        public DateTime? Fecha_Emision { get; set; }
+    }
+}
